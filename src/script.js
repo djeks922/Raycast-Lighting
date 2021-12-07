a82ddbb21367e8374a3781fce62248fa4054e75d
djeks922 <djeks922@gmail.com>
2021-12-07 11:36:09 +0400

    shaders setup configured, raycasting , plane, and light mesh are also added

--- src/script.js
+++ src/script.js
@@ -1,20 +1,18 @@
 import "./style.css";
 import * as THREE from "three";
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
- 
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import vertex from './shaders/vertex.glsl'
+import fragment from './shaders/fragment.glsl'
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
 
-
-
-
 // CURL NOISE FUNC
 
-const SimplexNoise = require('simplex-noise'),
-    simplex = new SimplexNoise(Math.random);
+const SimplexNoise = require("simplex-noise"),
+  simplex = new SimplexNoise(Math.random);
 
 function computeCurl(x, y, z) {
   var eps = 0.0001;
@@ -53,49 +51,99 @@ function computeCurl(x, y, z) {
   return curl;
 }
 
-
 /**
  * Object
  */
 
+//Curve generator
 const getCurve = (start) => {
-    let points = [];
-    points.push(start)
+  let scale= 2
+  let points = [];
+  points.push(start);
 
-    let currentPoint = start.clone();
-    for (let i = 0; i < 20; i++) {
-        // console.log(currentPoint)
-        let v = computeCurl(currentPoint.x,currentPoint.y,currentPoint.z)
-        currentPoint.addScaledVector(v, 0.001)
-        // console.log(currentPoint.clone(),v)
+  let currentPoint = start.clone();
+  for (let i = 0; i < 300; i++) {
+    // console.log(currentPoint)
+    let v = computeCurl(currentPoint.x/scale, currentPoint.y/scale, currentPoint.z/scale);
+    currentPoint.addScaledVector(v, 0.001);
+    // console.log(currentPoint.clone(),v)
 
-        points.push(currentPoint.clone())
-        
-    }
+    points.push(currentPoint.clone());
+  }
 
-    return points;
+  return points;
+};
+// TUBE generator
+const generateTubes = () => {
+  let path = null
+  let geometry =null
+  // const geometry2 = new THREE.PlaneBufferGeometry(1,1)
+  let material = new THREE.ShaderMaterial({
+    uniforms: {
+      uTime: {value : 0.0}
+    },
+    vertexShader: vertex,
+    fragmentShader: fragment, 
+
+    side: THREE.DoubleSide,
+  });
+  let mesh =null
+  const objects = []
+
+  for (let i = 0; i < 100; i++) {
+    path = new THREE.CatmullRomCurve3(
+      getCurve(
+        new THREE.Vector3(
+          Math.random() - 0.5,
+          Math.random() - 0.5,
+          Math.random() - 0.5
+        )
+      )
+    );
+    geometry = new THREE.TubeBufferGeometry(path, 100, 0.005, 8, false);
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+    objects.push(mesh)
+  }
+
 }
 
 
+// Raycast Func
+const raycast = () => {
+    
+    let raycastPlane = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(10,10),
+      new THREE.MeshBasicMaterial({color: 0xff0000})
+    )
+    let light= new THREE.Mesh(
+      new THREE.SphereBufferGeometry(0.1,20,20),
+      new THREE.MeshBasicMaterial({color: 0x00ff00})
+    )
+    scene.add(raycastPlane,light);
 
 
-let path ;
-let geometry ;
-// const geometry2 = new THREE.PlaneBufferGeometry(1,1)
-let material = new THREE.MeshNormalMaterial({
-    side: THREE.DoubleSide
-});
-let mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-const generateTubes = () =>{
-    for (let i = 0; i < 100; i++) {
-        path = new THREE.CatmullRomCurve3(getCurve(new THREE.Vector3(Math.random()-0.5,Math.random()-0.5,Math.random()-0.5)));
-        geometry = new THREE.TubeBufferGeometry(path ,30,0.005,8,false)
-        mesh = new THREE.Mesh(geometry, material);
-        scene.add(mesh);
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
+
+    window.addEventListener('mousemove', onMouseMove)
+    function onMouseMove(event ) {
+      
+      mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+      mouse.y = -(event.clientY/ window.innerHeight)* 2 - 1
+
+      raycaster.setFromCamera(mouse,camera)
+  
+      const intersects = raycaster.intersectObjects([raycastPlane])
+      if(intersects.length > 0){
+        let p  = intersects[0].point
+        // console.log(p)
+      } 
+
     }
 }
-generateTubes()
+
+
 
 /**
  * Sizes
@@ -127,7 +175,9 @@ scene.add(camera);
  *  Controls
  */
 
-const controls = new OrbitControls(camera,canvas);
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.update()
 /**
  * Renderer
  */
@@ -136,11 +186,23 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
-renderer.setClearColor(new THREE.Color('white'))
+renderer.setClearColor(new THREE.Color("black"));
+
+
+/**
+ *  Main
+ */
+
+ generateTubes();
+ raycast();
+
+/**
+ *  Animation
+ */ 
 
 const animate = () => {
   renderer.render(scene, camera);
-
+  controls.update();
   window.requestAnimationFrame(animate);
 };
 
---
+++ src/shaders/fragment.glsl
@@ -0,0 +1,9 @@
+
+varying vec2 vUv;
+varying vec3 vPosition;
+void main () {
+
+    gl_FragColor= vec4 (vUv.x,0.0,0.0,1.0);
+
+
+}
